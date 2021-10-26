c7b0b25450e8b86c0d53b2d50a918c292288d559
Mmh011004 <798459867@qq.com>
2021-10-26 17:55:14 +0800

    TreeFragment

--- app/src/main/java/com/example/wanandroidclient/ui/fragment/tree/AskFragment.kt
+++ app/src/main/java/com/example/wanandroidclient/ui/fragment/tree/AskFragment.kt
@@ -1,23 +1,22 @@
 package com.example.wanandroidclient.ui.fragment.tree
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.wanandroidclient.R
 import com.example.wanandroidclient.app.base.BaseFragment
 import com.example.wanandroidclient.databinding.IncludeListBinding
 import com.example.wanandroidclient.viewmodel.state.TreeViewModel
 
-
-class SystemFragment : BaseFragment<TreeViewModel, IncludeListBinding>() {
+/**
+ * 作者　: mmh
+ * 时间　: 2021/10/26
+ * 描述　:
+ */
+class AskFragment : BaseFragment<TreeViewModel, IncludeListBinding>(){
     override fun layoutId(): Int {
         return R.layout.include_list
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
     }
-
-
 }
--- app/src/main/java/com/example/wanandroidclient/ui/fragment/tree/TreeArrFragment.kt
+++ app/src/main/java/com/example/wanandroidclient/ui/fragment/tree/TreeArrFragment.kt
@@ -1,6 +1,7 @@
 package com.example.wanandroidclient.ui.fragment.tree
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.wanandroidclient.R
 import com.example.wanandroidclient.app.base.BaseFragment
 import com.example.wanandroidclient.databinding.FragmentViewpagerBinding
@@ -12,6 +13,19 @@ import com.example.wanandroidclient.viewmodel.state.TreeViewModel
  * 描述　:
  */
 class TreeArrFragment : BaseFragment<TreeViewModel, FragmentViewpagerBinding>() {
+
+    //标题
+    val titleData = arrayListOf("广场", "每日一问", "体系", "导航")
+    //Fragment集合
+    private var fragments : ArrayList<Fragment> = arrayListOf()
+
+    //添加四个Fragment
+    init {
+        fragments.add(PlazaFragment())
+        fragments.add(AskFragment())
+        fragments.add(SystemFragment())
+        fragments.add(NavigationFragment())
+    }
     override fun layoutId(): Int = R.layout.fragment_viewpager
 
     override fun initView(savedInstanceState: Bundle?) {
